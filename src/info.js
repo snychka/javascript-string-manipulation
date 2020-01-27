f85bb92bd450c1100c3964a001293d3e3a251961
AJ Foster <web@aj-foster.com>
2020-01-26 23:58:15 -0500

    Use a template literal for multi-line help text

--- src/info.js
+++ src/info.js
@@ -1,15 +1,22 @@
 var usage = "Usage: crlf-convert [CR|LF|CRLF]";
 
-var help = "Usage: crlf-convert [CR|LF|CRLF]\n\n"
-         + "Converts text between newline (\\n), carriage return (\\r), and combined\n"
-         + "(\\r\\n) line endings.\n\n"
-         + "Options:\n\n"
-         + "[CR|LF|CRLF]: Desired line ending\n\n"
-         + "    CR is a carriage return character \\r (rare)\n"
-         + "    LF is a line feed character \\n (common on Unix and macOS)\n"
-         + "    CRLF is a combined ending \\r\\n (common on Windows)\n"
-         + "Example:\n\n"
-         + "    cat in.txt | crlf-convert LF > out.txt";
+const help = `Usage: crlf-convert [CR|LF|CRLF]
+
+Converts text between newline (\\n), carriage return (\\r), and combined
+(\\r\\n) line endings.
+
+Options:
+
+[CR|LF|CRLF]: Desired line ending
+
+    CR is a carriage return character \\r (rare)
+    LF is a line feed character \\n (common on Unix and macOS)
+    CRLF is a combined ending \\r\\n (common on Windows)
+
+Example:
+
+    cat in.txt | crlf-convert LF > out.txt
+`;
 
 module.exports = {
   help,
