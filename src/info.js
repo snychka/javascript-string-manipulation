ef1ce6d9aae2770b590339fba7e353c2559fec20
AJ Foster <web@aj-foster.com>
2020-01-26 23:58:49 -0500

    Use String.raw for handling escaped characters

--- src/info.js
+++ src/info.js
@@ -1,17 +1,17 @@
 var usage = "Usage: crlf-convert [CR|LF|CRLF]";
 
-const help = `Usage: crlf-convert [CR|LF|CRLF]
+const help = String.raw`Usage: crlf-convert [CR|LF|CRLF]
 
-Converts text between newline (\\n), carriage return (\\r), and combined
-(\\r\\n) line endings.
+Converts text between newline (\n), carriage return (\r), and combined
+(\r\n) line endings.
 
 Options:
 
 [CR|LF|CRLF]: Desired line ending
 
-    CR is a carriage return character \\r (rare)
-    LF is a line feed character \\n (common on Unix and macOS)
-    CRLF is a combined ending \\r\\n (common on Windows)
+    CR is a carriage return character \r (rare)
+    LF is a line feed character \n (common on Unix and macOS)
+    CRLF is a combined ending \r\n (common on Windows)
 
 Example:
 
